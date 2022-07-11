b8cbfe8e7c9323ffa73259c124447d899b106d06
bryan vera <bveracachay@gmail.com>
2022-07-11 11:56:58 -0500

    se agrega clases

--- menu_principal/main.js
+++ menu_principal/main.js
@@ -1,12 +1,22 @@
 const dateTime = document.querySelector('.container-profile__info__time-date');
 const userName = document.querySelector('.container-profile__info__name');
-
-userName.innerHTML += ' ' + 'John Doe';
-
-setInterval(clock, 1000);
-
-function clock() {
-  const date = new Date();
-  dateTime.innerHTML =
-    date.toLocaleDateString() + ' ' + date.toLocaleTimeString();
+class ChangeObject {
+  constructor(name) {
+    this.name = name;
+  }
+  getName() {
+    userName.innerHTML += ' ' + this.name;
+  }
+  clock() {
+    const date = new Date();
+    dateTime.innerHTML =
+      date.toLocaleDateString() + ' ' + date.toLocaleTimeString();
+  }
 }
+let probando = new ChangeObject('john doe');
+
+setInterval(() => {
+  probando.clock();
+}, 1000);
+
+probando.getName();
